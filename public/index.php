a3f5b165a80b8d7e2d629eb1f8f934aa63603895
chomark <chomark@wemakeprice.com>
2014-01-15 16:09:19 +0900

    indexºбâó¸®

--- public/index.php
+++ public/index.php
@@ -4,20 +4,25 @@
 	 * make man : chomark
 	 * make tools : phalcon
 	 * * */
-	 $config = new Phalcon\Config\Adapter\Ini('../app/config/config.ini');
+	 try{
+	 	 $config = new Phalcon\Config\Adapter\Ini('../app/config/config.ini');
 	 
-	 //로더 설정
-	 $load = new \Phalcon\Loader();
-	 $load->registerDirs(
-			 	array(
-			 		$config->application->controllersDir,
-			 		$config->application->pluginsDir,
-			 		$config->application->libraryDir,
-			 		$config->application->modelsDir,
-				)
-			 )->register();
-	 
-	$app = new \Phalcon\Mvc\Application($di);
-	echo $app->handle()->getContent(); 
+		 //로더 설정
+		 $load = new \Phalcon\Loader();
+		 $load->registerDirs(
+				 	array(
+				 		$config->application->controllersDir,
+				 		$config->application->pluginsDir,
+				 		$config->application->libraryDir,
+				 		$config->application->modelsDir,
+					)
+				 )->register();
+		 
+		$app = new \Phalcon\Mvc\Application($di);
+		echo $app->handle()->getContent(); 
+	 }catch(\Phalcon\Exception $e){
+	 	echo "PhalconException: ", $e->getMessage();
+	 }
+	
 	
 ?>
